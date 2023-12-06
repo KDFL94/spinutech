841314843af2a0cb407e491f5f7dd8cc4cb8f373
Cadron Neal <cadronneal@Cadrons-MBP-2.lan>
2023-12-06 13:49:54 -0500

    Finished project

---
+++ README.md
@@ -0,0 +1,5 @@
+### Cadron's Test Project for Spinutech
+
+Just run `npm run build`
+
+Thanks for the oppurtunity I hope we move forward
--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,47 @@
-// Imported jQuery
+// Import jQuery
 window.$ = window.jQuery = require('jquery');
 
 $(() => {
-  // initialize
-  console.log('ok i loaded');
+
+  /**
+   *  Here is a mouse enter event.
+   *  This handles when the icon/shape
+   *  is hovered.
+   */
+  $('.a-icon-wrap').on('mouseenter', (event) => {
+    // Targets the icon wrap and adds an active class to it
+    $(event.target).closest('.a-icon-wrap').addClass('active');
+
+    // Sets the icon wraps opacity to 50% except for the active one
+    $('.a-icon-wrap').not('.active').css('opacity', '50%');
+  });
+
+  /**
+   *  Here is a mouse leave event.
+   *  This handles when the icon/shape
+   *  is exited.
+   */
+  $('.a-icon-wrap').on('mouseleave', (event) => {
+    // This removes the opacity set
+    $('.a-icon-wrap').css('opacity', '');
+
+    // Removes the active class from the active icon wrap
+    $(event.target).closest('.a-icon-wrap').removeClass('active');
+  });
+
+  /**
+   *  Hear is the icon click event.
+   *  This handles when the icon/shape 
+   *  is clicked it will add the clicked 
+   *  icon to the correct row
+   */
+  $('.a-icon-wrap').on('click', () => {
+    // This conditional checks the row of the active class
+    if ($('.active').parent().attr('id') == 'row-1') {
+      $('.active').prependTo('#row-2');
+    }
+    else {
+      $('.active').prependTo('#row-1');
+    }
+  });
 });
